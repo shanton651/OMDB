e1cc581caba551888ffa09009689cd712852d386
NathanAlkurdi <65264477+NathanAlkurdi@users.noreply.github.com>
2020-07-04 20:49:04 -0500

    Improvements to movie_info, added relation lists for movie_data and movie_songs, and added table for movie/year summary
    Additions/Improvements:
    - Merged changes from professor's UI
    - Added poster to movie_info.php
    - Cleaned up code for movie_info.php
    - Responsive web design implementation for people information in movie_info.php
    - Made table for movie_data and movie_song relation. Requires the correct query to work.
    - Made table for movie/year summary. Requires the correct query to work.
    - All posters should be stored in /movie_media/ and m_link should only be the image file name such as 'forrest_gump_poster.jpg' in order to render correctly.
    
    Removed:
    - db_credentials.php is merged with database.php now

--- db_credentials.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-DEFINE('DB_SERVER', 'localhost');
-DEFINE('DB_NAME', 'omdb');
-DEFINE('DB_USER', 'root');
-DEFINE('DB_PASS', '');
-
-?>
--- movies_extended_relation.php
+++ movies_extended_relation.php
@@ -14,14 +14,111 @@
 
       <h2 style = "color: #01B0F1;">Movies: Extended Relation</h3>
 
-      </br>
+        </br>
 
-      <h4>To be done later.</h4>
+        <table id="info" cellpadding="0" cellspacing="0" border="0"
+            class="datatable table table-striped table-bordered datatable-style table-hover"
+            width="100%" style="width: 100px;">
+              <thead>
+                <tr id="table-first-row">
+                        <th>ID</th>
+                        <th>Native Name</th>
+                        <th>English Name</th>
+                        <th>Year</th>
+                        <th>Language</th>
+                        <th>Country</th>
+                        <th>Genre</th>
+                        <th>Plot</th>
+                </tr>
+              </thead>
 
-    </div>
+              <tfoot>
+                <tr>
+                        <th>ID</th>
+                        <th>Native Name</th>
+                        <th>English Name</th>
+                        <th>Year</th>
+                        <th>Language</th>
+                        <th>Country</th>
+                        <th>Genre</th>
+                        <th>Plot</th>                       
+                </tr>
+              </tfoot>
 
-</div>
+              <tbody>
+
+              <?php
+
+$sql = "SELECT * from `movies` ORDER BY year_made ASC;";
+$result = $db->query($sql);
+
+                if ($result->num_rows > 0) {
+                    // output data of each row
+                    while($row = $result->fetch_assoc()) {
+                        echo '<tr>
+                                <td>'.$row["movie_id"].'</td>
+                                <td>'.$row["native_name"].'</td>
+                                <td>'.$row["english_name"].' </span> </td>
+                                <td>'.$row["year_made"].'</td>
+                                <td>'.$row["language"].'</td>
+                                <td>'.$row["country"].'</td>
+                                <td>'.$row["genre"].'</td>
+                                <td>'.$row["plot"].'</td>                             
+                            </tr>';
+                    }//end while
+                }//end if
+                else {
+                    echo "0 results";
+                }//end else
+
+                 $result->close();
+                ?>
+
+              </tbody>
+        </table>
 
 
+        <script type="text/javascript" language="javascript">
+    $(document).ready( function () {
+        
+        $('#info').DataTable( {
+            dom: 'lfrtBip',
+            buttons: [
+                'copy', 'excel', 'csv', 'pdf'
+            ] }
+        );
 
-  <?php include("./footer.php"); ?>
+        $('#info thead tr').clone(true).appendTo( '#info thead' );
+        $('#info thead tr:eq(1) th').each( function (i) {
+            var title = $(this).text();
+            $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
+    
+            $( 'input', this ).on( 'keyup change', function () {
+                if ( table.column(i).search() !== this.value ) {
+                    table
+                        .column(i)
+                        .search( this.value )
+                        .draw();
+                }
+            } );
+        } );
+    
+        var table = $('#info').DataTable( {
+            orderCellsTop: true,
+            fixedHeader: true,
+            retrieve: true
+        } );
+        
+    } );
+
+</script>
+
+        
+
+ <style>
+   tfoot {
+     display: table-header-group;
+   }
+ </style>
+
+<?php include("./footer.php"); ?>
