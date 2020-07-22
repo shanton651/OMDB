ec103ed55d326cf655edf7c746f8c6e24f0a834a
Abdullahi <66758149+Abdullahi117@users.noreply.github.com>
2020-07-22 17:25:58 -0500

    added song sql
    song sql added

--- sql/views_iteration8.sql
+++ sql/views_iteration8.sql
@@ -50,3 +50,39 @@ FROM   `movies`
                         GROUP  BY movie_id ASC) m_people 
                     ON movies.movie_id = m_people.movie_id 
 GROUP  BY movies.movie_id ASC; 
+
+--------------- 
+---------------Song View
+
+SELECT `songs`.`song_id`            AS `song_id`, 
+       `songs`.`title`              AS `title`, 
+       `songs`.`lyrics`             AS `lyrics`, 
+       `songs`.`theme`              AS `theme`, 
+       `song_media`.`song_media_id` AS `song_media_id`, 
+       `song_media`.`s_link`        AS `s_link`, 
+       `song_media`.`s_link_type`   AS `s_link_type`, 
+       `song_media`.`song_id`       AS `song_id`, 
+       `song_people`.`song_id`      AS `song_id`, 
+       `song_people`.`people_id`    AS `people_id`, 
+       `song_people`.`role`         AS `role`, 
+       `people`.`people_id`         AS `people_id`, 
+       `people`.`stage_name`        AS `stage_name`, 
+       `people`.`first_name`        AS `first_name`, 
+       `people`.`last_name`         AS `last_name`, 
+       `people`.`gender`            AS `gender`, 
+       `movie_song`.`movie_id`      AS `movie_id`, 
+       `movie_song`.`song_id`       AS `song_id`, 
+       `movies`.`movie_id`          AS `movie_id`, 
+       `movies`.`native_name`       AS `native_name` 
+FROM   ( ( ( ( ( `songs` 
+                 LEFT JOIN `song_media` 
+                        ON ( `songs`.`song_id` = `song_media`.`song_id` ) ) 
+               LEFT JOIN `song_people` 
+                      ON ( `songs`.`song_id` = `song_people`.`song_id` ) ) 
+             LEFT JOIN `people` 
+                    ON ( `song_people`.`people_id` = `people`.`people_id` ) ) 
+           LEFT JOIN `movie_song` 
+                  ON ( `song_people`.`song_id` = `movie_song`.`song_id` ) ) 
+         LEFT JOIN `movies` 
+                ON ( `movie_song`.`movie_id` = `movies`.`movie_id` ) ) 
+GROUP  BY `songs`.`song_id`;
