c12ab4521f17b15a6404dbf012411fdc62e4d191
Gina Torres <gina.bjork@my.metrostate.edu>
2020-07-22 20:13:04 -0500

    Fixed movie_view and song_view

--- sql/views_iteration8.sql
+++ sql/views_iteration8.sql
@@ -25,27 +25,27 @@ FROM   `movies`
                         GROUP  BY movie_id ASC) trivia 
                     ON movies.movie_id = trivia.movie_id 
        LEFT OUTER JOIN (SELECT movie_id, 
-                               Group_concat(movie_id) AS `quotes` 
+                               COUNT(movie_id) AS `quotes` 
                         FROM   `movie_quotes` 
                         GROUP  BY movie_id ASC) quotes
                     ON movies.movie_id = quotes.movie_id 
        LEFT OUTER JOIN (SELECT movie_id, 
-                               Group_concat(movie_id) AS `keywords` 
+                               COUNT(movie_id) AS `keywords` 
                         FROM   `movie_keywords` 
                         GROUP  BY movie_id ASC) keywords 
                     ON movies.movie_id = keywords.movie_id 
        LEFT OUTER JOIN (SELECT movie_id, 
-                               Group_concat(movie_id) AS `media` 
+                               COUNT(movie_id) AS `media` 
                         FROM   `movie_media` 
                         GROUP  BY movie_id ASC) media 
                     ON movies.movie_id = media.movie_id 
        LEFT OUTER JOIN (SELECT movie_id, 
-                               Group_concat(movie_id) AS `songs` 
+                               COUNT(movie_id) AS `songs` 
                         FROM   `movie_song` 
                         GROUP  BY movie_id ASC) song 
                     ON movies.movie_id = song.movie_id 
        LEFT OUTER JOIN (SELECT movie_id, 
-                               Group_concat(movie_id) AS `people` 
+                               COUNT(movie_id) AS `people` 
                         FROM   `movie_people` 
                         GROUP  BY movie_id ASC) m_people 
                     ON movies.movie_id = m_people.movie_id 
