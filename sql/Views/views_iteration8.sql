b0c3fb8ca814e4c6edcaf1e3b944eebeb9aad925
Gina Torres <gina.bjork@my.metrostate.edu>
2020-07-29 22:16:29 -0500

    Fixed movies view

--- sql/Views/views_iteration8.sql
+++ sql/Views/views_iteration8.sql
@@ -1,55 +1,50 @@
 -----------
 -- Movie View
 
-SELECT movies.movie_id, 
-       movies.native_name, 
-       movies.english_name, 
-       movies.year_made, 
-       movie_data.tag_line, 
-       movie_data.language, 
-       movie_data.country, 
-       movie_data.genre, 
-       movie_data.plot, 
-       trivia,
-       quotes, 
-       keywords, 
-       media, 
-       songs, 
-       people
-FROM   `movies` 
-       LEFT OUTER JOIN `movie_data` 
-                    ON movies.movie_id = movie_data.movie_id 
-       LEFT OUTER JOIN (SELECT movie_id, 
-                               Group_concat(movie_id) AS `trivia` 
-                        FROM   `movie_trivia` 
-                        GROUP  BY movie_id ASC) trivia 
-                    ON movies.movie_id = trivia.movie_id 
-       LEFT OUTER JOIN (SELECT movie_id, 
-                               COUNT(movie_id) AS `quotes` 
-                        FROM   `movie_quotes` 
-                        GROUP  BY movie_id ASC) quotes
-                    ON movies.movie_id = quotes.movie_id 
-       LEFT OUTER JOIN (SELECT movie_id, 
-                               COUNT(movie_id) AS `keywords` 
-                        FROM   `movie_keywords` 
-                        GROUP  BY movie_id ASC) keywords 
-                    ON movies.movie_id = keywords.movie_id 
-       LEFT OUTER JOIN (SELECT movie_id, 
-                               COUNT(movie_id) AS `media` 
-                        FROM   `movie_media` 
-                        GROUP  BY movie_id ASC) media 
-                    ON movies.movie_id = media.movie_id 
-       LEFT OUTER JOIN (SELECT movie_id, 
-                               COUNT(movie_id) AS `songs` 
-                        FROM   `movie_song` 
-                        GROUP  BY movie_id ASC) song 
-                    ON movies.movie_id = song.movie_id 
-       LEFT OUTER JOIN (SELECT movie_id, 
-                               COUNT(movie_id) AS `people` 
-                        FROM   `movie_people` 
-                        GROUP  BY movie_id ASC) m_people 
-                    ON movies.movie_id = m_people.movie_id 
-GROUP  BY movies.movie_id ASC; 
+SELECT movies.movie_id,
+       movies.native_name,
+       movies.english_name,
+       movies.year_made,
+       movie_data.tag_line,
+       movie_data.language,
+       movie_data.country,
+       movie_data.genre,
+       movie_data.plot,
+       GROUP_CONCAT(distinct movie_trivia_name) AS movie_trivia_names,
+       GROUP_CONCAT(distinct keyword) AS keywords,
+       GROUP_CONCAT(distinct m_link) AS media_link,
+       GROUP_CONCAT(distinct m_link_type) AS media_link_type,
+       GROUP_CONCAT(distinct title) AS titles,
+       GROUP_CONCAT(distinct stage_name),
+       movie_numbers.running_time,
+       movie_numbers.length,
+       movie_numbers.strength,
+       movie_numbers.weight,
+       movie_numbers.budget,
+       movie_numbers.box_office,
+       movie_anagrams.anagram
+FROM movies
+       LEFT JOIN movie_data 
+       ON (movies.movie_id = movie_data.movie_id)
+       LEFT JOIN movie_trivia 
+       ON (movies.movie_id = movie_trivia.movie_id)
+       LEFT JOIN movie_keywords 
+       ON (movies.movie_id = movie_keywords.movie_id)
+       LEFT JOIN movie_media 
+       ON (movies.movie_id = movie_media.movie_id)
+       LEFT JOIN movie_song 
+       ON (movies.movie_id = movie_song.song_id)
+       LEFT JOIN songs 
+       ON (movie_song.song_id = songs.song_id)
+       LEFT JOIN movie_people 
+       ON (movies.movie_id = movie_people.movie_id)
+       LEFT JOIN people 
+       ON (movie_people.people_id = people.people_id)
+       LEFT JOIN movie_numbers 
+       ON (movies.movie_id = movie_numbers.movie_id)
+       LEFT JOIN movie_anagrams 
+       ON (movies.movie_id = movie_anagrams.movie_id)
+GROUP BY movies.movie_id;
 
 --------------- 
 ---------------Song View
