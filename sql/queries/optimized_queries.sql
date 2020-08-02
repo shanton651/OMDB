584db19e7fe5f660000709b66528c5d81bbfed9a
NathanAlkurdi <65264477+NathanAlkurdi@users.noreply.github.com>
2020-08-02 16:34:39 -0500

    Combined Optimized queries
    Added my query to optimized_queries.sql and spencer's movie to the insert in Bears_Queries.sql

---
+++ sql/queries/optimized_queries.sql
@@ -0,0 +1,32 @@
+-----------------------------------------------------------
+-- Optimized SQL Queries
+-----------------------------------------------------------
+
+-----------------------------------------------------------
+-- Query to find a movie based on a certain song's title
+-- By: Nathan Al-Kurdi
+-----------------------------------------------------------
+
+-- BEFORE
+----------------------------------------------------
+
+SELECT * 
+FROM   (movies 
+        NATURAL JOIN movie_song) 
+       NATURAL JOIN songs 
+WHERE  movies.movie_id = movie_song.movie_id 
+       AND songs.title LIKE 'Jungle%' 
+
+-- AFTER (With indexed attributes)
+----------------------------------------------------
+
+SELECT song_id, 
+       movie_id, 
+       title AS song_title, 
+       native_name, 
+       year_made 
+FROM   (movies 
+        NATURAL JOIN movie_song) 
+       NATURAL JOIN songs 
+WHERE  movies.movie_id = movie_song.movie_id 
+       AND songs.title LIKE 'Jungle%' 
