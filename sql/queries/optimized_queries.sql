c3cf29e605977a275706fe00463be68233f06b6a
Gina Torres <gina.bjork@my.metrostate.edu>
2020-08-02 18:26:23 -0500

    Added my optimized query

--- sql/queries/optimized_queries.sql
+++ sql/queries/optimized_queries.sql
@@ -30,3 +30,25 @@ FROM   (movies
        NATURAL JOIN songs 
 WHERE  movies.movie_id = movie_song.movie_id 
        AND songs.title LIKE 'Jungle%' 
+
+
+
+
+-----------------------------------------------------------
+-- Query to add a song into the songs table
+-- By: Gina Bjork
+-----------------------------------------------------------
+-- BEFORE
+----------------------------------------------------
+
+INSERT INTO songs (title, lyrics, theme) 
+VALUES ("Her Eyes", NULL, "Classic")
+
+
+-- AFTER
+----------------------------------------------------
+
+INSERT INTO songs (title, lyrics, theme) 
+SELECT title, lyrics, theme 
+FROM songs;
+
