9024d503c3caa5672a0c9001fdec77dfebaf1f4d
Christian <nr7256bm@go.minnstate.edu>
2020-08-05 11:18:01 -0500

    I have added my optimized query
    I chose to do query number 45. I optimized it by adding an index on
    stage_name I detailed this in a comment in the optimized_queries.sql
    file.

--- sql/queries/optimized_queries.sql
+++ sql/queries/optimized_queries.sql
@@ -52,3 +52,28 @@ INSERT INTO songs (title, lyrics, theme)
 SELECT title, lyrics, theme 
 FROM songs;
 
+
+
+-----------------------------------------------------------------------------
+-- Query to Lists all movies where person x or person y appear (put stage
+-- names in the '')
+-- By: Christian Duvick
+-----------------------------------------------------------------------------
+
+SELECT movies.native_name 
+FROM `movies`
+WHERE movie_id IN (SELECT movie_id
+                  FROM movie_people NATURAL JOIN people
+                  WHERE people.stage_name = '' OR people.stage_name = '')
+
+
+-- AFTER
+----------------------------------------------------
+-- Note to optimize this query I did not change the query itself I added a
+-- index on stage name with this: ALTER TABLE `omdb`.`people` ADD INDEX `stage_name_index` (`stage_name`);
+
+SELECT movies.native_name 
+FROM `movies`
+WHERE movie_id IN (SELECT movie_id
+                  FROM movie_people NATURAL JOIN people
+                  WHERE people.stage_name = '' OR people.stage_name = '')
